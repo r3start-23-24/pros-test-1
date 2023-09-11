fbebc4ed6a32bd06ca399b98d4a874ac7d2af214
drnez <96260951+drnez@users.noreply.github.com>
2023-09-11 20:25:27 +0100

    starr extern

---
+++ include/robot.hpp
@@ -0,0 +1,9 @@
+#ifndef ROBOT_HPP
+
+#include "api.h"
+#include "pros/motors.hpp"
+
+extern pros::Motor catapult;
+
+#define ROBOT_HPP
+#endif
---
+++ src/robot.cpp
@@ -0,0 +1,3 @@
+#include "robot.hpp"
+
+pros::Motor catapult (6, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES);
