8e3cd3836136197e58a4f6be676c3735a4103459
drnez <shayannezami@yahoo.co.uk>
2023-09-11 20:40:23 +0100

    extern file finished

--- include/robot.hpp
+++ include/robot.hpp
@@ -3,7 +3,21 @@
 #include "api.h"
 #include "pros/motors.hpp"
 
-extern pros::Motor catapult;
+extern pros::Controller mainController;
+
+extern pros::Motor left_motor_1;
+extern pros::Motor left_motor_2;
+extern pros::Motor right_motor_1;
+extern pros::Motor right_motor_2;
+extern pros::Motor_Group left_drive_motors;
+extern pros::Motor_Group right_drive_motors;
+
+extern pros::Motor cata_motor;
+extern pros::ADIAnalogIn cata_limit_switch;
+
+extern pros::Motor intake_motor_1;
+extern pros::Motor intake_motor_2;
+extern pros::Motor_Group intake_motors;
 
 #define ROBOT_HPP
 #endif
--- src/robot.cpp
+++ src/robot.cpp
@@ -1,3 +1,19 @@
 #include "robot.hpp"
 
-pros::Motor catapult (6, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Controller mainController (pros::E_CONTROLLER_MASTER);
+
+pros::Motor left_motor_1 (2, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor left_motor_2 (3, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor right_motor_1 (8, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor right_motor_2 (9, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor_Group left_drive_motors ({left_motor_1, left_motor_2});
+pros::Motor_Group right_drive_motors ({right_motor_1, right_motor_2});
+
+pros::Motor cata_motor (6, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES);
+//pros::Motor cata_motor_2 (6, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
+//pros::Motor_Group cata_motors ({cata_motor_1, cata_motor_2});
+pros::ADIAnalogIn cata_limit_switch('A');
+
+pros::Motor intake_motor_1 (4, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor intake_motor_2 (7, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor_Group intake_motors ({intake_motor_1, intake_motor_2});
