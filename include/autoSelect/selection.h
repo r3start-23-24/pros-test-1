b1e3bde83479730e381578f0c9188a337633c86d
drnez <shayannezami@yahoo.co.uk>
2023-11-14 21:10:23 +0000

    change selector readings

--- include/autoSelect/selection.h
+++ include/autoSelect/selection.h
@@ -2,10 +2,21 @@
 
 #include <string>
 
+/* 
+  selector::auton == 1 : Red Front
+  selector::auton == 2 : Red Back
+  selector::auton == 3 : Do Nothing
+  selector::auton == -1 : Blue Front
+  selector::auton == -2 : Blue Back
+  selector::auton == -3 : Do Nothing
+  selector::auton == 0 : Skills
+*/
+
 //selector configuration
 #define HUE 360
 #define DEFAULT 1
-#define AUTONS "Front", "Back", "Do Nothing"
+#define AUTONS "same goal", "other goal", "nothing"
+// 1, 2, 3
 
 namespace selector{
 
