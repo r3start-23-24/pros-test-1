58261e8389d2ae04ddf39db2911e26f42a876fcf
drnez <shayannezami@yahoo.co.uk>
2023-11-22 17:37:13 +0000

    added 2 extra motors for 6m drive

--- include/robot.hpp
+++ include/robot.hpp
@@ -7,8 +7,10 @@ extern pros::Controller mainController;
 
 extern pros::Motor left_motor_1;
 extern pros::Motor left_motor_2;
+extern pros::Motor left_motor_3;
 extern pros::Motor right_motor_1;
 extern pros::Motor right_motor_2;
+extern pros::Motor right_motor_3;
 extern pros::Motor_Group left_drive_motors;
 extern pros::Motor_Group right_drive_motors;
 
