856793a7bcb5c4c556691f33864d13d8e0b7c3c0
drnez <96260951+drnez@users.noreply.github.com>
2023-12-07 13:17:19 +0000

    UNTUNED awp done

--- include/autoSelect/selection.h
+++ include/autoSelect/selection.h
@@ -3,8 +3,8 @@
 #include <string>
 
 /* 
-  selector::auton == 1 : Red same goal
-  selector::auton == 2 : Red other goal
+  selector::auton == 1 : Red same goal POINTS
+  selector::auton == 2 : Red other goal AWP
   selector::auton == 3 : Do Nothing
   selector::auton == -1 : Blue same goal
   selector::auton == -2 : Blue other goal
@@ -14,7 +14,7 @@
 
 //selector configuration
 #define HUE 360
-#define DEFAULT 0
+#define DEFAULT 2
 #define AUTONS "same goal", "other goal", "nothing"
 // 1, 2, 3
 
