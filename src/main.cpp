e555263a2d186d60f565a66cfa72215635abae48
Shayan Nezami <shayannezami@yahoo.co.uk>
2024-02-15 15:48:40 +0000

    did points auton framework

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,12 @@
 #include "main.h"
-#include "gif-pros/gifclass.hpp"
 #include "lemlib/chassis/chassis.hpp"
 #include "lemlib/util.hpp"
 #include "pros/misc.h"
 #include "pros/motors.h"
 #include "pros/rtos.h"
-#include "pros/rtos.hpp"
 #include "robot.hpp"
 #include "autons.hpp"
 #include "autoSelect/selection.h"
-#include "lemlib/api.hpp"
 
 bool front_left_wing_extended = false;
 bool front_right_wing_extended = false;
@@ -62,20 +59,6 @@ void turnRight(float degrees, int velocity) {
 	}
 }
 
-const float one_lemlib_tile = 24;
-float lemlib_x;
-float lemlib_y;
-void move_lemlib(float tiles, char direction, int velocity = 90) {
-    direction == 'x' ? lemlib_x += one_lemlib_tile*tiles : lemlib_y += one_lemlib_tile*tiles;
-    lemlib_chassis.moveTo(lemlib_x, lemlib_y, 3000, velocity);
-}
-void turn_lemlib(int alpha, int velocity = 50) {
-    int theta = lemlib_chassis.getPose().theta;
-    theta = lemlib::degToRad(theta);
-    alpha = lemlib::degToRad(alpha);
-    lemlib_chassis.turnTo(lemlib_x - sin(theta + alpha), lemlib_y + cos(theta + alpha), 1000);
-}
-
 void initialize() {
 	lemlib_chassis.calibrate();
     lemlib_chassis.setPose(0,0,0);
@@ -101,24 +84,14 @@ void autonomous() {
             skills_auton();
             break;
         case 1:
+            points_auton();
             break;
         case 2:
+            awp_auton();
             break;
         default:
             break;
     }
-	intake_motor.move(127);
-	move_lemlib(2.25, 'y', 90);
-	intake_motor.move(0);
-	lemlib_chassis.turnTo(-100, 2.5*one_lemlib_tile, 1000, 60);
-	front_left_wing.set_value(true);
-	intake_motor.move(-127);
-	move_lemlib(1, 'x', 90);
-	front_left_wing.set_value(false);
-	//move_lemlib(-0.5, 'x', 90);
-	lemlib_chassis.moveTo(0.4*one_lemlib_tile, 2.25*one_lemlib_tile, 2000, 80);
-	lemlib_chassis.moveTo(-one_lemlib_tile+10, one_lemlib_tile/2, 3000, 60);
-	lemlib_chassis.turnTo(-24, 5, 1000, true, 60);
 }
 
 void drive_loop() {
